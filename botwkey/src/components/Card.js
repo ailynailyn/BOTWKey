bf07d67c642c2e37808f04aedc4ad28d93caacfd
Ailyn Aguirre <ailyndarlenne@gmail.com>
2022-01-11 02:02:33 -0700

    active carousel card css

--- botwkey/src/components/Card.js
+++ botwkey/src/components/Card.js
@@ -1,3 +1,5 @@
+import "./Card.css";
+
 const Card = ({ handlePointerEvent, name, image, cardStyle }) => {
   return (
     <article className={cardStyle}>
@@ -6,8 +8,28 @@ const Card = ({ handlePointerEvent, name, image, cardStyle }) => {
         onMouseDown={handlePointerEvent}
         onTouchStart={handlePointerEvent}
       >
-        <img src={image} alt={name} />
-        <h2>{name}</h2>
+        <div className="card-header">
+          <p>{name}</p>
+          <img src={image} alt={name} />
+        </div>
+        <div className="card-middle">
+          <div className="card-hearts">
+            <img src={image} alt={name} />
+          </div>
+          <div className="card-image">
+            <img src={image} alt={name} />
+          </div>
+          <div className="card-stats">
+            <img src={image} alt={name} />
+          </div>
+        </div>
+        <div className="card-footer">
+          <div className="footer-title">
+            <p>DESCRIPTION</p>
+            <p>DROPS</p>
+            <p>COMMON LOCATIONS</p>
+          </div>
+        </div>
       </div>
     </article>
   );
