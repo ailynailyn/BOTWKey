6920a522f95c4397025583ad4f14ead083c5a267
Ailyn Aguirre <ailyndarlenne@gmail.com>
2022-01-14 12:28:12 -0700

    Link updates

--- botwkey/src/containers/CreaturesContainer.js
+++ botwkey/src/containers/CreaturesContainer.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import App from './App';
+import React from "react";
 
 function CreaturesContainer() {
   return (
--- botwkey/src/index.js
+++ botwkey/src/index.js
@@ -4,16 +4,22 @@ import "./index.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import HomeContainer from "./containers/HomeContainer";
 import HomeScreenContainer from "./containers/HomeScreenContainer";
+import CreaturesContainer from "./containers/CreaturesContainer";
+import MonstersContainer from "./containers/MonstersContainer";
+import MaterialsContainer from "./containers/MaterialsContainer";
+import EquipmentContainer from "./containers/EquipmentContainer";
+import TreasureContainer from "./containers/TreasureContainer";
+
 ReactDOM.render(
   <Router>
     <Routes>
       <Route path="/" element={<HomeScreenContainer />} />
       <Route path="/carousel" element={<HomeContainer />} />
-      <Route path="/creatures" element={<HomeContainer />} />
-      <Route path="/monsters" element={<HomeContainer />} />
-      <Route path="/materials" element={<HomeContainer />} />
-      <Route path="/equipment" element={<HomeContainer />} />
-      <Route path="/treasure" element={<HomeContainer />} />
+      <Route path="/creatures" element={<CreaturesContainer />} />
+      <Route path="/monsters" element={<MonstersContainer />} />
+      <Route path="/materials" element={<MaterialsContainer />} />
+      <Route path="/equipment" element={<EquipmentContainer />} />
+      <Route path="/treasure" element={<TreasureContainer />} />
     </Routes>
   </Router>,
 
