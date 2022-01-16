2d4e1c4147ee90d51f72309a4246bd970b0de605
Ailyn Aguirre <ailyndarlenne@gmail.com>
2022-01-15 20:20:51 -0700

    dynamic on click routing to correct link

--- botwkey/src/containers/ItemsTableContainer.js
+++ botwkey/src/containers/ItemsTableContainer.js
@@ -1,16 +1,26 @@
 import React from "react";
 import "./ItemsTableContainer.css";
 import { Card, Row, Col } from "react-bootstrap";
+import { Navigate, useNavigate } from "react-router-dom";
 
 function ItemsTableContainer(props) {
   const data = props.data;
+  let navigate = useNavigate();
+
+  function clickCard(category, itemId) {
+    navigate("/" + category + "/" + itemId);
+  }
+
   return (
     <div className="ItemsTableContainer">
       <Row xs={4}>
         {data.map((curItem, n) => {
           return (
             <Col>
-              <Card className="tableItemCard">
+              <Card
+                className="tableItemCard"
+                onClick={() => clickCard(curItem.category, curItem.id)}
+              >
                 <Card.Img variant="top" src={curItem.image} />
                 <Card.Body>
                   <Card.Title>{curItem.name}</Card.Title>
