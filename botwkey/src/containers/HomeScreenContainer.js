e82fa3633d3cd0ab9ba27465f517e9d40054faf2
LAPTOP-FJECQ0R7\Aleksya <aaguir31@gmail.com>
2022-02-06 15:34:32 -0700

    added rotating hover effect to all ruppees and adjusted overlay text

--- botwkey/src/containers/HomeScreenContainer.js
+++ botwkey/src/containers/HomeScreenContainer.js
@@ -10,6 +10,7 @@ import card_backside2 from "../images/materials_ruppee.png";
 import card_backside3 from "../images/creatures_ruppee.png";
 import card_backside4 from "../images/monsters_ruppee.png";
 import card_backside5 from "../images/treasure_ruppee.png";
+import treasure_gif from "../images/treasure_gif.gif";
 import header_bgimage from "../images/header_image.png";
 import header_logoimage from "../images/homepage_title.png";
 import "./HomeScreenContainer.css";
@@ -49,53 +50,28 @@ function HomeScreenContainer() {
       </div>
       <div className="body">
         <div className="cardGroupContainer">
-          <div class="item">
+          <div class="item_creatures">
             <div class="ruppee-card" onClick={clickCreatures}>
-              <img
-                class="card__img"
-                src={card_backside3}
-                alt="Category Creatures"
-              />
               <div class="overlay">CREATURES</div>
             </div>
           </div>
-          <div class="item">
+          <div class="item_monsters">
             <div class="ruppee-card" onClick={clickMonsters}>
-              <img
-                class="card__img"
-                src={card_backside4}
-                alt="Category Monsters"
-              />
               <div class="overlay">MONSTERS</div>
             </div>
           </div>
-          <div class="item">
+          <div class="item_materials">
             <div class="ruppee-card" onClick={clickMaterials}>
-              <img
-                class="card__img"
-                src={card_backside2}
-                alt="Category Materials"
-              />
               <div class="overlay">MATERIALS</div>
             </div>
           </div>
-          <div class="item">
+          <div class="item_equipment">
             <div class="ruppee-card" onClick={clickEquipment}>
-              <img
-                class="card__img"
-                src={card_backside1}
-                alt="Category Equipment"
-              />
               <div class="overlay">EQUIPMENT</div>
             </div>
           </div>
-          <div class="item">
+          <div class="item_treasure">
             <div class="ruppee-card" onClick={clickTreasure}>
-              <img
-                class="card__img"
-                src={card_backside5}
-                alt="Category Treasure"
-              />
               <div class="overlay">TREASURE</div>
             </div>
           </div>
